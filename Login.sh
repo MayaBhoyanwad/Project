126ecedfca7beb11475ea2ab932687246ec4b763
MayaBhoyanwad <141413829+MayaBhoyanwad@users.noreply.github.com>
2023-08-27 23:07:43 +0530

    Update Login.sh

--- Login.sh
+++ Login.sh
@@ -2,12 +2,10 @@
 valid_username="abc"
 valid_password="123"
 
-# Prompt user for input
 read -p "Username: " input_username
 read -s -p "Password: " input_password
 echo
 
-# Check if provided credentials are valid
 if [ "$input_username" == "$valid_username" ] && [ "$input_password" == "$valid_password" ];
 then
     echo "Login successful"
